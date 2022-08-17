7859e0579d4e0a4a6c7239acd8f21eb735f1d1d3
AlinaMytsa <act.mitsa@gmail.com>
2022-08-17 21:25:13 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
 'use strict';
 
-const mainFunction = (exponentiation) => {
+// const mainFunction = (exponentiation) => {
+//
+//   const number = +prompt('enter the number pls');
+//   const degree = +prompt('enter the degree pls');
+//
+//   exponentiation(number,degree)
+// }
+//
+// const exponentiation = (number, degree) => {
+//   const result = Math.pow(number, degree);
+//   return alert(result);
+// }
+// mainFunction(exponentiation);
 
-  const number = +prompt('enter the number pls');
-  const degree = +prompt('enter the degree pls');
 
-  exponentiation(number,degree)
+// 2
+
+// const mainFunction = (multiplay) => {
+//
+//   const num1 = +prompt('enter the first number pls');
+//   const num2 = +prompt('enter the second number pls');
+//
+//   multiplay(num1, num2)
+// }
+//
+// const multiplay = (num1, num2) => {
+//   const result = num1 * num2;
+//   return alert(result)
+// }
+// mainFunction(multiplay);
+
+
+//3
+
+// const mainFunction = (division) => {
+//   const num1 = +prompt('enter the first number pls');
+//   const num2 = +prompt('enter the second number pls');
+//
+//   division(num1, num2);
+// }
+// const division = (num1, num2) => {
+//   const result = num1 / num2;
+//   return alert(result);
+// }
+//
+// mainFunction(division);
+
+
+
+//4
+const mainFunction = (modulo) => {
+  const num1 = +prompt('enter the first number pls');
+  const num2 = +prompt('enter the second number pls');
+
+  modulo(num1, num2)
 }
 
-const exponentiation = (number, degree) => {
-  const result = Math.pow(number, degree);
+const modulo = (num1, num2) => {
+  const result = num1 % num2;
   return alert(result);
 }
-mainFunction(exponentiation);
+mainFunction(modulo);
